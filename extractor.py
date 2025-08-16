c00af5d6803ed032c2e93c38b6849eaf16135273
adamlaycock <adam.m.laycock@gmail.com>
2025-08-16 19:11:03 +0100

    add START_ITEM constant

--- extractor.py
+++ extractor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 YEARS = [2012, 2013, 2014, 2015]
+START_ITEM = 'Country'
 
 md_df = pd.read_excel(
     r"C:\Users\adamm\OneDrive\Documents\VSC\Projects\ECDC AMR Surveillance\source_data\Acinetobacter, Tables 3-21 to 3-25.xlsx",
@@ -14,7 +15,7 @@ metadata = md_df.at[0, 0]
 
 col_names = md_df.iloc[0,:]
 col_names = [col_name.strip() for col_name in col_names]
-new_names = ['Country']
+new_names = [START_ITEM]
 for year in YEARS:
     for name in col_names[1:4]:
         if name != 'Country':
@@ -33,13 +34,13 @@ df = pd.read_excel(
 
 df.columns = new_names
 
-print(df)
+# df = df.melt(
+#     id_vars='Country',
+#     var_name='MetricYear',
+#     value_name='Value'
+# )
 
-df = df.melt(
-    id_vars='Country',
-    var_name='MetricYear',
-    value_name='Value'
-)
+# df[['Metric', 'Year']] = df['MetricYear'].str.split('_', expand=True)
+# df = df.drop(columns='MetricYear')
 
-df[['Metric', 'Year']] = df['MetricYear'].str.split('_', expand=True)
-df = df.drop(columns='MetricYear')
+df
