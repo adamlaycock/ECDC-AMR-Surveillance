f08ba244d52d9a659fb44ea5f2e7e33007adacc3
adamlaycock <adam.m.laycock@gmail.com>
2025-08-16 19:06:07 +0100

    add extractor.py

---
+++ extractor.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+YEARS = [2012, 2013, 2014, 2015]
+
+md_df = pd.read_excel(
+    r"C:\Users\adamm\OneDrive\Documents\VSC\Projects\ECDC AMR Surveillance\source_data\Acinetobacter, Tables 3-21 to 3-25.xlsx",
+    sheet_name="Table 3.21",
+    header=None,
+    skipfooter=3,
+    skiprows=3
+)
+
+metadata = md_df.at[0, 0]
+
+col_names = md_df.iloc[0,:]
+col_names = [col_name.strip() for col_name in col_names]
+new_names = ['Country']
+for year in YEARS:
+    for name in col_names[1:4]:
+        if name != 'Country':
+            new_names.append(f"{name}_{year}")
+
+
+
+
+
+df = pd.read_excel(
+    r"C:\Users\adamm\OneDrive\Documents\VSC\Projects\ECDC AMR Surveillance\source_data\Acinetobacter, Tables 3-21 to 3-25.xlsx",
+    sheet_name="Table 3.21",
+    skipfooter=3,
+    skiprows=3
+)
+
+df.columns = new_names
+
+print(df)
+
+df = df.melt(
+    id_vars='Country',
+    var_name='MetricYear',
+    value_name='Value'
+)
+
+df[['Metric', 'Year']] = df['MetricYear'].str.split('_', expand=True)
+df = df.drop(columns='MetricYear')
